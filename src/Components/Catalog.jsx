3d412c8bd37fad98cd4d2cc4cc8bf0095ca2b5ee
Altay Hodoglugil <altay.hodo@gmail.com>
2025-01-13 15:59:48 -0800

    set up fakestoreapi

--- src/Components/Catalog.jsx
+++ src/Components/Catalog.jsx
@@ -1,3 +1,22 @@
+import { useState, useEffect } from 'react';
+
 export default function Catalog() {
+  const [items, setItems] = useState(null);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const response = await fetch('https://fakestoreapi.com/products');
+        const data = await response.json();
+        console.log(data);
+        setItems(data.slice(0, 6));
+      } catch (error) {
+        alert('Error fetching items', error);
+      }
+    };
+
+    fetchItems();
+  }, []);
+
   return <div className="catalog">Catalog</div>;
 }
--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -7,7 +7,7 @@ export default function HomePage(){
         Welcome to my E-Commerce Website!
       </div>
       <div className={styles.description}>
-        We are a lorem ipsum dolor sit amet consectetur adipisicing elit. Aut dicta repellendus, voluptas quis, exercitationem illum culpa voluptate optio placeat minima possimus quo dolore porro rem corporis ut, quae qui iste?
+        We are a website that lorem ipsum dolor sit amet consectetur adipisicing elit. Aut dicta repellendus, voluptas quis, exercitationem illum culpa voluptate optio placeat minima possimus quo dolore porro rem corporis ut, quae qui iste. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis explicabo iure officiis est iste, quis voluptatem quam beatae delectus aliquam alias eum ab animi eaque non, quas, maiores vero repudiandae!
       </div>
     </div>
   )
